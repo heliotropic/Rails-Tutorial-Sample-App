e8ca4b3f1bf603f12b07d6bf8b876390baddf712
Matt McNeil <dev@mattmcneil.org>
2012-02-20 18:58:02 -0800

    added help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,14 @@ describe "Static pages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
+      page.should have_content('Home')
+    end
+  end
+  describe "Help page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
     end
   end
 end
