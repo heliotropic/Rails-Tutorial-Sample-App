773de2d6a39c55aa439e66ff4f11d568eb6bd38e
Matt McNeil <dev@mattmcneil.org>
2012-02-21 14:56:50 -0800

    updated static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+  def about
+  end
+  def contact
+  end
 
   def help
   end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,19 +2,57 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/pages/home'
-      page.should have_content('Sample App')
-      page.should have_content('Home')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/pages/home'
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
+
   describe "Help page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Help'" do
       visit '/pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/pages/help'
+      page.should have_selector('title', :text => "#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About'" do
+      visit '/pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/pages/about'
+      page.should have_selector('title', :text => "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end
 end
